c09f5f0f4d0680ee9879842d84512cb67f28a095
Bruno Alves <alves.bruno945@gmail.com>
2021-03-06 17:01:26 -0300

    Initial Commit

---
+++ AluraViagens/AluraViagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  AluraViagensApp.swift
+//  AluraViagens
+//
+//  Created by Bruno Silva on 06/03/21.
+//
+
+import SwiftUI
+
+@main
+struct AluraViagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
