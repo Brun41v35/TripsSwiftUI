797c7c2167cb163d5da56a11277af74b6d21bb3f
Bruno Alves <alves.bruno945@gmail.com>
2021-03-07 11:12:54 -0300

    Initial Commit

--- AluraViagens/ContentView.swift
+++ AluraViagens/ContentView.swift
@@ -9,12 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { view in
+            
+            //MARK: - VStack Principal
+            VStack {
+                
+                //MARK: - VStack Header
+                VStack{
+                    Text("Alura Viagens")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, 50)
+                    Text("ESPECIAL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    Text("BRASIL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 23))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(Color.purple)
+                .edgesIgnoringSafeArea(.all)
+                
+                List {
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Atibaia")
+                    Text("São Paulo")
+                }
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct showPreview: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
